40296d7f1e747d6b8ab969b6e567e7a5d486cfa4
Alberto Vila <alberto.vilatena@gmail.com>
2017-10-01 00:39:04 +0300

    A basic way of returning the attacks in the last 72 hrs

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,15 @@
 import { Response, Request } from "express";
+import * as request from "request";
+import * as filter from "lodash.filter";
+import * as moment from "moment";
+
+const API_KEY = 'c780598790867245b643f77ab1d8825f7f8d0671d9c194bdd87a383b9366d45c'
 
 export let index = (req: Request, res: Response) => {
-    res.json({
-        message: 'Welcome to Phishglobe API'
+    request.get(`http://data.phishtank.com/data/online-valid.json`, function (error, response, body) {
+        let data = filter(JSON.parse(body), function (phish) {
+            return moment(phish.submission_time).isAfter(moment().subtract(3, 'days').utc())
+        })
+        res.json(data)
     })
 }

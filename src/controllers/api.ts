8c18b0c7f6712d2c6f20451661c301939cbd45d8
Alberto Vila <alberto.vilatena@gmail.com>
2017-10-08 14:31:49 +0300

    Return IP location from backend

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,15 +1,26 @@
 import { Response, Request } from "express";
+import { Phish } from "../models/Phish"
 import * as request from "request";
 import * as filter from "lodash.filter";
+import * as map from "lodash.map";
 import * as moment from "moment";
 
 const API_KEY = 'c780598790867245b643f77ab1d8825f7f8d0671d9c194bdd87a383b9366d45c'
 
 export let index = (req: Request, res: Response) => {
     request.get(`http://data.phishtank.com/data/online-valid.json`, function (error, response, body) {
-        let data = filter(JSON.parse(body), function (phish) {
+        let objects = JSON.parse(body);
+        let data = filter(objects, function (phish) {
             return moment(phish.submission_time).isAfter(moment().subtract(3, 'days').utc())
+        });
+        let promises = Array<Promise<Phish>>();
+
+        map(data, function (phish) {
+            promises.push(Phish.process(phish))
         })
-        res.json(data)
+
+        Promise.all(promises).then(function (results) {
+            res.json(results)
+        })
     })
 }
---
+++ src/models/Phish.ts
@@ -0,0 +1,45 @@
+import * as request from "request";
+import { Moment } from "moment";
+import * as map from "lodash.map";
+
+export class Phish {
+    private phish_id: number;
+    private url: string;
+    private phish_detail_url: string;
+    private submission_time: string;
+    private verified: boolean;
+    private online: boolean;
+    private target: string;
+    private lat: number;
+    private lon: number;
+
+    constructor(phish: Object) {
+        this.phish_id = +phish['phish_id'];
+        this.url = phish['url'];
+        this.phish_detail_url = phish['phish_detail_url'];
+        this.submission_time = phish['submission_time'];
+        this.verified = phish['verified'] === 'yes';
+        this.online = phish['online'] === 'yes';
+        this.target = phish['target'];
+    }
+
+    public static process(phish: Object): Promise<Phish> {
+        let ip = phish['details'][0]['ip_address']
+        return new Promise(function (resolve, reject) {
+            let p = new Phish(phish);
+
+            request.get(`https://ipinfo.io/${ip}/json`, function (error, response, body) {
+                if (error) {
+                    return reject(error);
+                }
+
+                let location = JSON.parse(body)["loc"].split(",");
+
+                p.lat = +location[0];
+                p.lon = +location[1];
+
+                resolve(p);
+            });
+        });
+    }
+}

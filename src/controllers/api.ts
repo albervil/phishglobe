fc4c8f06bb92f8d8c905d4429f9c32e86e7ab7f7
Alberto Vila <alberto.vilatena@gmail.com>
2017-10-13 21:48:27 +0300

    Retrieve the data in a script

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,20 +3,9 @@ import { Phish } from "../models/Phish"
 import * as request from "request";
 import * as moment from "moment";
 import * as JSONStream from "jsonstream";
+import * as fs from "fs";
 
 export let index = (req: Request, res: Response) => {
-    let results = Array<Phish>();
-    
-    request.get(`http://data.phishtank.com/data/online-valid.json`)
-        .pipe(JSONStream.parse('*'))
-        .on('data', function (phish) {
-            if (moment(phish.submission_time).isAfter(moment().subtract(3, 'days').utc())) {
-                Phish.process(phish).then(function(p){
-                    results.push(p);
-                })
-            }
-        })
-        .on('end', function () {
-            res.json(results);
-        })
+    fs.createReadStream("./resources/72hrs-online-valid.json")
+        .pipe(res);
 }
